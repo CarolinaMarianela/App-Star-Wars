34cd7a62b11a8e1baf50c362081b32d1c10dc7f8
Carolina Marianela <gallegos.carolina@outlook.com>
2022-09-29 11:39:19 +0100

    add cards

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({ movie, onSetShowModal, onSelectedCard }) => {
+  return (
+    <div
+      onClick={() => {
+        onSetShowModal(true);
+        onSelectedCard(movie);
+      }}
+      className="cursor-pointer bg-white w-1/3 h-2/3 p-8 rounded-lg m-8 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+    >
+      <div className="justify-center text-3xl text-black">{movie.title}</div>
+    </div>
+  );
+};
+export default Card;
